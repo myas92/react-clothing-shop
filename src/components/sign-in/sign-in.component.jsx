12b59aeec81261e82e8a95cf3d5541e6031eaede
Yaser Ahmadi <m.y.ahmadi22@gmail.com>
2021-09-23 20:26:32 +0330

    finishing sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import CustomButtom from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 
 // import { SignInWithGoogle } from '../../firebase/firebase.utils';
-import { SignInWithGoogle } from '../../firebase/firebase.utils';
+import {auth, SignInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -17,9 +17,15 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit =async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' })
+        const {email, password} = this.state
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange = event => {

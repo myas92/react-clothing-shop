421ff68ddb1cbb933c10682bbde7271f1be3834e
mohammadyaser <m.y.ahmadi22@gmail.com>
2021-10-28 23:00:34 +0330

    improving naming of component(category to collection)

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+import 'collection.styles.scss'
+import CollectionItem from '../../components/collection-item/collection-item.component';
+ 
+const CollectionPage = ({match}) => (
+    <div className="collection">
+        <h2>collection Page</h2>
+
+    </div>
+)
+
+export default CollectionPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,13 +4,13 @@ import './shop.styles.scss'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import { Route } from 'react-router';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 //match:{ isExact, params, path:"/shop" , url}
 const ShopPage = ({ match }) => (
     <div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+        <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
     </div>
 )
 

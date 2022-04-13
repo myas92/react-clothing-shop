d70c6f5996a926deb3a1242642c9b164d3c6cf26
yaser ahmadi <m.y.ahmadi22@gmail.com>
2022-04-13 14:54:57 +0430

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-
 import logger from 'redux-logger';
-
 import rootreducer from './root-reducer';
 
 const middlewares = []
